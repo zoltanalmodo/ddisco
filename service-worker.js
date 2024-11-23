3b660c2d9b0b661bc56cfb276cb6d6836e2f09ed
zoltan almodo <zoltanalmodo@gmail.com>
2024-11-23 18:23:59 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/doodle-disco-app-002/precache-manifest.95ce9385cae99a4ff4e8554243e1f79c.js"
+  "/doodle-disco-app-002/precache-manifest.7f0b6496c98a25e04a080322caab83e8.js"
 );
 
 self.addEventListener('message', (event) => {
